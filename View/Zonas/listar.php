051f9b647c1ba7acf2f67a833756cf5b385f04b8
Esteban De La Fuente Rubio <esteban@delaf.cl>
2014-04-03 13:27:34 -0300

    Quitado App::uses()

--- View/Zonas/listar.php
+++ View/Zonas/listar.php
@@ -5,7 +5,6 @@ foreach ($zonas as &$zona) {
     $zona[] = '<a href="editar/'.$zona['id'].'"><img src="'.$_base.'/img/icons/16x16/actions/edit.png" alt="" /></a>';
 }
 array_unshift ($zonas, array('ID', 'Zona', 'RD Class', 'DNSSEC', 'Acciones'));
-App::uses ('MaintainerHelper', 'View/Helper');
 $t = new \sowerphp\app\View_Helper_Maintainer (array('link'=>$_base.'/bind10/zonas'), false);
 $t->setId ('zonas');
 echo $t->listar($zonas);
